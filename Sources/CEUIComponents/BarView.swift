7b1ae69e8860c861b518837e4424841d74806593
Çağatay Emekci <cemekci@atlassian.com>
2020-04-05 01:41:15 +0300

    barview was added

---
+++ Sources/CEUIComponents/BarView.swift
@@ -0,0 +1,39 @@
+//
+//  BarView.swift
+//  CEUIComponents
+//
+//  Created by Çağatay Emekci on 5.04.2020.
+//
+import SwiftUI
+
+public class BarObject: ObservableObject {
+    @Published var barWith: CGFloat = 0
+    @Published var backColor: UIColor = .red
+    @Published var barColor: UIColor = .blue
+}
+
+public struct BarView: View  {
+    @ObservedObject var bar: BarObject
+    public init(bar: BarObject){
+        self.bar = bar
+    }
+    public var body: some View {
+        GeometryReader { geometry in
+            ZStack (alignment: .leading) {
+                HStack {
+                    Rectangle()
+                        .fill(Color(self.bar.backColor))
+                        .frame(width: geometry.size.width, height: 10.0)
+                        .cornerRadius(3)
+                }
+                HStack {
+                    Rectangle()
+                        .fill(Color(self.bar.barColor))
+                        .frame(width: geometry.size.width * (self.bar.barWith / 100), height: 10.0)
+                        .cornerRadius(3)
+                }.animation(Animation.easeOut(duration: 1).delay(1))
+            }
+
+        }
+    }
+}
